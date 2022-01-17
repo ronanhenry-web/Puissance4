25d272c07ed1a25a6f0d2b66f1bcefaa21738de9
ronan <ronanhenry@orange.fr>
2022-01-17 20:45:39 +0100

    Projet p4

---
+++ app.js
@@ -0,0 +1,217 @@
+const ROWS = 6; //ligne
+const COLUMNS = 7; //colonne
+const IN_A_ROW = 4; //puissance 4
+const PLAYER_COLORS = ["", "red", "yellow"]; //couleur joueur
+let tableau = []; //tableau grille
+let currentPlayerTurn = 1; //tour joueur
+let maGrilleHTML = document.getElementById("grille"); //affiche le tableau
+let htmlGrid = ""; //tableau cellule
+let pseudo_Joueur = [""]; //pseudo joueur
+
+//Déclaration tableau
+//Changement de joueur 1 et 2
+function ChangerTour() {
+	let tourJ = document.getElementById("tourJ");
+	if(currentPlayerTurn == 1) {
+		currentPlayer
+for (let i = 0; i < ROWS; i++) {
+  tableau.push([]);
+  for (let j = 0; j < COLUMNS; j++) {
+    tableau[i].push(0);
+  }
+}
+Turn = 2;
+		tourJ.innerHTML = currentPlayerTurn; //Changement tour
+	} else {
+		currentPlayerTurn = 1;
+		tourJ.innerHTML = currentPlayerTurn; //Changement tour
+	}
+}
+
+//Définit le tableau avec le HTML
+//Récupère la div "grille" dans le HTML
+for (let i = 0; i < ROWS; i++) {
+	//Ouverture de la balise ROWS
+	htmlGrid += '<div class="row">';
+	for (let j = 0; j < COLUMNS; j++) {
+		let cellID = i + "," + j;
+		//Cellule COLUMNS et numérote les cellules
+		htmlGrid += '<div class="cell" id = "'+ cellID +'"></div>';
+	}
+	//Fermeture de la balise ROWS
+	htmlGrid += "</div>";
+}
+//Donne la grille
+maGrilleHTML.innerHTML = htmlGrid
+
+//Evènement touche digit et enter
+function Touche(event) {
+	if(event.key == "Enter") {
+		reset();
+	}
+
+	let keyCode = event.code;
+	if(!keyCode.includes("Digit")) {
+		//Mauvaise touche
+		return
+	}
+	//Bonne touche
+	let characterIndex = keyCode.length - 1;
+	let columnString = keyCode.substring(characterIndex);
+	let column = parseInt(columnString);
+	insertionJeton(column - 1);
+  	//console.log(keyCode); //Test
+}
+document.addEventListener("keydown", Touche);
+
+//Jetons dans la grille
+function insertionJeton(column) {
+	for(let i = 0; i < tableau.length; i++) {
+		//Deux conditions bas case prise et limite haut
+		if (i + 1 >= ROWS || tableau[i + 1][column] != 0 && tableau[i][column] == 0) {
+			//Drop jeton par rapport au tour de jeu
+			tableau[i][column] = currentPlayerTurn;
+			//Appel fonction
+			insertionHTMLJeton(i, column);
+			verificationVictoire(i, column);
+			ChangerTour();
+			MatchNul();
+		break;
+		}
+	}
+	return true;
+}
+
+//Valeur différente pour les joueurs avec les couleurs rouge et jaune
+function insertionHTMLJeton(red, yellow) {
+	const currentPlayerColor = PLAYER_COLORS[currentPlayerTurn]
+	document.getElementById(red + "," + yellow).classList.add(
+		currentPlayerColor);
+}
+
+//Vérification victoire base principale
+function verificationVictoire(row, column) {
+	for (let i = 0; i < ROWS; i++) {
+		for(let j = 0; j < COLUMNS; j++) {
+			if(HorizontalAlignment(i, j) ||
+				VerticalAlignment(i, j) ||
+				DiagonalLeftAlignment(i, j) ||
+				DiagonalRightAlignment(i, j)) {
+				//Affichage pour l'utilisateur
+				alert("Bravo le joueur : " + currentPlayerTurn + " a gagné !!!");
+				alert("Partie terminée appuyer sur ENTER pour recommencer");
+				return true;
+			}
+		}
+	}
+	//Retourne rien
+	return false;
+}
+
+//Vérification victoire horizontal
+function HorizontalAlignment(row, column){
+	if(column + IN_A_ROW > COLUMNS) {
+		return false;
+	}
+	for(let i = 0; i < IN_A_ROW; i++) {
+		if(tableau[row][column + i] != currentPlayerTurn) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//Vérification victoire vertical
+function VerticalAlignment(row, column){
+	if(row + IN_A_ROW > ROWS) {
+		return false;
+	}
+	for(let i = 0; i < IN_A_ROW; i++) {
+		if(tableau[row + i][column] != currentPlayerTurn) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//Vérification victoire diagonal droite
+function DiagonalRightAlignment(row, column){
+	if(column + IN_A_ROW > COLUMNS || row + IN_A_ROW > ROWS) {
+		return false;
+	}
+	for(let i = 0; i < IN_A_ROW; i++) {
+		if(tableau[row + i][column + i] != currentPlayerTurn) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//Vérification victoire diagonal gauche
+function DiagonalLeftAlignment(row, column){
+	if(column - (IN_A_ROW - 1) < 0 || row + IN_A_ROW > ROWS) {
+		return false;
+	}
+	for(let i = 0; i < IN_A_ROW; i++) {
+		if(tableau[row + i][column - i] != currentPlayerTurn) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//Match nul
+function MatchNul() {
+	for(let i = 0; i < ROWS; i++) {
+		for(let j = 0; j < COLUMNS; j++) {
+			if(tableau[i][j] == 0) {
+				return false;
+			}
+		}
+	}
+	//Affichage pour l'utilisateur
+	alert("Egalité dommage ☺");
+	return true;
+}
+
+//Permet d'enlever les jetons en liaison avec la fonction event
+function reset() {
+	let gameEnded = event.key;
+	if(gameEnded && event.key == "Enter") {
+		//Reset les jetons en parcourant la grille et supprimant les valeurs/la couleur & changement de joueur
+		for (let i = 0; i < ROWS; i++) {
+			for(let j = 0; j < COLUMNS; j++) {
+				tableau[i][j] = 0;
+				document.getElementById(i + "," + j)
+				.classList.remove("red");
+				tableau[i][j] = 0;
+				document.getElementById(i + "," + j)
+				.classList.remove("yellow");
+			}
+		}
+		//console.log(currentPlayerTurn); //Test
+		ChangerTour(false);
+		//console.log(currentPlayerTurn); /Test
+	}	
+//console.log(tableau) //Test
+//console.log(gameEnded); //Test
+}
+
+ //Interface joueur demande un pseudo et l'affiche dans un bloc
+function choixPseudo() {
+	var txt;
+	var personne1 = prompt("Insérer votre pseudo", "Joueur 1 : ");
+	if(personne1 == null || personne1 == "") {
+		txt = "Pseudo invalide";
+	}
+	var personne2 = prompt("Insérer votre pseudo", "Joueur 2 : ");
+	if(personne1 == null || personne1 == "") {
+		txt = "Pseudo invalide";
+	} else {
+		txt = "Joueur 1 : " + personne1 + " " + "VS" + " " + "Joueur 2 : " + personne2
+	}
+	document.getElementById("insertionPseudoJoueur").innerHTML = txt;
+
+	//Permet d'afficher le tour de joueur
+	document.getElementById("tourJ").innerHTML = pseudo_Joueur[currentPlayerTurn];
+}
